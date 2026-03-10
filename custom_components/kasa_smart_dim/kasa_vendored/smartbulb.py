79375d37fd18fa62cda74f6573aa48a53cb3dbcd
pslawinski <pslawinski@gmail.com>
2026-03-09 19:57:37 -0500

    Stuff

--- custom_components/kasa_smart_dim/kasa_vendored/smartbulb.py
+++ custom_components/kasa_smart_dim/kasa_vendored/smartbulb.py
@@ -69,7 +69,7 @@ class TurnOnBehavior(BaseModel):
     """
 
     #: Index of preset to use, or ``None`` for the last known state.
-    index: int = Field(default=None)
+    index: int | None = Field(default=None)
     #: Wanted behavior
     mode: BehaviorMode = BehaviorMode.Last
 
